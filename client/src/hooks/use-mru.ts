5b87a11d47fa53d2c9fe7ec1b4257a729d9291b1
Mike Murphy <michael.w.murphy@outlook.com>
2025-01-27 00:38:35 +0000

    Adds a hook for persisting most recently used items in local storage

---
+++ client/src/hooks/use-mru.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface MostRecentlyUsed {
+  groups: string[];
+  locations: string[];
+}
+
+const KEY = "quantum:mru";
+const DEFAULT_MRU: MostRecentlyUsed = {
+  groups: [],
+  locations: [],
+}
+
+export function useMostRecentlyUsed() {
+  const [mru, setMru] = useState<MostRecentlyUsed>(() => {
+    try {
+      const data = localStorage.getItem(KEY);
+      return data ? JSON.parse(data) : DEFAULT_MRU;
+    } catch {
+      console.warn("Failed to load MRU from local storage");
+      setMru(DEFAULT_MRU);
+      return DEFAULT_MRU;
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem(KEY, JSON.stringify(mru));
+  }, [mru]);
+
+  function addGroupToFavourites(groupKey: string) {
+    if (mru.groups.includes(groupKey)) return;
+    setMru((prev) => ({
+      ...prev,
+      groups: [...prev.groups, groupKey],
+    }));
+  }
+
+  return {
+    recentGroups: mru.groups,
+    recentLocations: mru.locations,
+    addGroupToFavourites,
+  }
+}
