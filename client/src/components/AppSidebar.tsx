4e5da5c6592bae2698025a140a3bc07f3fc79cdc
Mike Murphy <michael.w.murphy@outlook.com>
2025-01-30 06:41:02 +0000

    Implements dark theme option

--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -1,4 +1,4 @@
-import { ChevronUp, Home, Package, Map, User, Settings, LogOut } from "lucide-react"
+import {ChevronUp, Home, Package, Map, User, Settings, LogOut, SunMoon, Sun, Moon} from "lucide-react"
 import {Link, useLocation} from "react-router"
 import {
   Sidebar,
@@ -20,11 +20,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuLabel,
   DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuPortal,
+  DropdownMenuSubContent,
 } from "./ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import { Badge } from "./ui/badge";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { useSettings } from "@/hooks/use-settings.tsx";
+import {Theme, useTheme} from "@/layouts/theme-provider.tsx";
+import * as React from "react";
 
 type SidebarItem = {
   title: string;
@@ -43,10 +50,17 @@ interface AppSidebarProps {
   userItems: UserTrackedItems;
 }
 
+const themes: { key: Theme, title: string, icon: React.FC }[] = [
+  { key: "system", title: "System", icon: SunMoon },
+  { key: "light", title: "Light", icon: Sun },
+  { key: "dark", title: "Dark", icon: Moon },
+];
+
 export function AppSidebar({ userItems: userOwnedItems }: AppSidebarProps) {
   const sidebar = useSidebar();
   const { terminology } = useSettings();
   const location = useLocation();
+  const { theme: currentTheme, setTheme } = useTheme();
 
   const items: SidebarItem[] = [
     { title: "Home", url: "/", icon: Home },
@@ -121,22 +135,59 @@ export function AppSidebar({ userItems: userOwnedItems }: AppSidebarProps) {
               </DropdownMenuTrigger>
               <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
                 <DropdownMenuLabel>Username</DropdownMenuLabel>
+
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <User className="w-5 h-5" />
-                  <span>Account</span>
-                  <DropdownMenuShortcut>⇧⌘A</DropdownMenuShortcut>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <Settings className="w-5 h-5" />
-                  <Link to="/settings" className="w-full">Settings</Link>
-                  <DropdownMenuShortcut>⇧⌘S</DropdownMenuShortcut>
-                </DropdownMenuItem>
+
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <User className="w-5 h-5" />
+                    <span>Account</span>
+                    <DropdownMenuShortcut>⇧⌘A</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <Settings className="w-5 h-5" />
+                    <Link to="/settings" className="w-full">Settings</Link>
+                    <DropdownMenuShortcut>⇧⌘S</DropdownMenuShortcut>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+
                 <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                  <LogOut className="w-5 h-5" />
-                  <span>Sign out</span>
-                </DropdownMenuItem>
+
+                <DropdownMenuGroup>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                      <SunMoon className="w-5 h-5" />
+                      <span>Select theme</span>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent>
+                        {themes.map((theme) => (
+                          <DropdownMenuItem
+                            key={theme.key}
+                            onClick={() => setTheme(theme.key)}
+                            className="cursor-pointer"
+                            disabled={theme.key === currentTheme}
+                          >
+                            <theme.icon />
+                            <div className="w-full flex justify-between items-center gap-2">
+                              <span>{theme.title}</span>
+                              {theme.key === currentTheme && <div className="block w-2 h-2 bg-border rounded-full"></div>}
+                            </div>
+                          </DropdownMenuItem>
+                        ))}
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                </DropdownMenuGroup>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                    <LogOut className="w-5 h-5" />
+                    <span>Sign out</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
               </DropdownMenuContent>
             </DropdownMenu>
           </SidebarMenuItem>
--- client/src/layouts/root-layout.tsx
+++ client/src/layouts/root-layout.tsx
@@ -3,6 +3,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar, UserTrackedItems } from "@/components/AppSidebar.tsx";
 import { useItemsApi } from "@/data/api/items";
 import { useEffect, useState } from "react";
+import {ThemeProvider} from "@/layouts/theme-provider.tsx";
 
 export default function RootLayout() {
   const { getUserTrackedItems } = useItemsApi();
@@ -14,9 +15,11 @@ export default function RootLayout() {
   }, []);
 
   return (
-    <SidebarProvider>
-      <AppSidebar userItems={userTrackedItems} />
-      <Outlet />
-    </SidebarProvider>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <SidebarProvider>
+        <AppSidebar userItems={userTrackedItems} />
+        <Outlet />
+      </SidebarProvider>
+    </ThemeProvider>
   )
 }
