a1e27ee0e91ae8d8906da0fb00bea3294368b11e
Mike Murphy <michael.w.murphy@outlook.com>
2025-01-30 18:02:10 +0000

    Fix icon for created item

--- client/src/pages/item-details/ItemHistoryCard.tsx
+++ client/src/pages/item-details/ItemHistoryCard.tsx
@@ -1,7 +1,7 @@
 import {ReactNode, useState } from "react";
 import {ItemCreatedEvent, ItemHistoryEvent, ItemTrackedEvent} from "@/data/models/item";
 import { Button } from "@/components/ui/button";
-import { ArrowDownFromLine, ChevronDown, ChevronUp } from "lucide-react";
+import { ArrowDownFromLine, Box, ChevronDown, ChevronUp } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -13,6 +13,8 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { format } from "date-fns";
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from "@/components/ui/collapsible";
 import { useSettings } from "@/hooks/use-settings.tsx";
+import {Link} from "react-router";
+import {cn} from "@/lib/utils.ts";
 
 interface ItemTrackHistoryProps {
   history: ItemHistoryEvent[];
@@ -78,26 +80,28 @@ function HistoryRow({ children }: { children: ReactNode }) {
   )
 }
 
-function HistoryUserIdentity({ userName, uniqueIdentifier }: { userName: string, uniqueIdentifier: string }) {
+function HistoryUserIdentity({ userName, uniqueIdentifier, children, className }: { userName: string, uniqueIdentifier: string, children: ReactNode, className?: string }) {
   return (
-    <div className="flex items-center gap-6">
+    <div className="flex items-center gap-6 overflow-hidden">
       <Avatar>
-        <AvatarFallback className="bg-purple-300/80"><ArrowDownFromLine strokeWidth={1} size={18} /></AvatarFallback>
+        <AvatarFallback className={cn("bg-purple-300/80", className)}>
+          {children}
+        </AvatarFallback>
       </Avatar>
       <div>
-        <p className="font-semibold tracking-wide">{userName}</p>
+        <p className="font-semibold tracking-wide" title={uniqueIdentifier}>{userName}</p>
         <p className="font-mono text-slate-600">{uniqueIdentifier}</p>
       </div>
     </div>
   )
 }
 
-function HistoryLocationDetail({ variant, locationName, date }: { variant: "created" | "tracked", locationName: string, date: string }) {
+function HistoryLocationDetail({ variant, locationName, locationId, date }: { variant: "created" | "tracked", locationName: string, locationId: string, date: string }) {
   const prefix = variant === "created" ? "Created in" : "Tracked to";
 
   return (
     <p className="sm:flex sm:flex-col text-muted-foreground">
-      <span className="sm:text-foreground">{prefix} <span className="sm:font-semibold">{locationName} </span></span>
+      <span className="sm:text-foreground">{prefix} <Link to={`/locations/${locationId}`} className="sm:font-semibold underline underline-offset-2 hover:no-underline">{locationName} </Link></span>
       <span className="sm:text-right sm:font-mono sm:tracking-tight">{format(date, "PPP HH:mm")}</span>
     </p>
   )
@@ -106,8 +110,10 @@ function HistoryLocationDetail({ variant, locationName, date }: { variant: "crea
 function ItemTrackedEventDetail({ event }: { event: ItemTrackedEvent }) {
   return (
     <HistoryRow>
-      <HistoryUserIdentity userName={event.userName} uniqueIdentifier="user.name@notimplemented.com" />
-      <HistoryLocationDetail variant="tracked" locationName={event.data.locationName} date={event.date} />
+      <HistoryUserIdentity userName={event.userName} uniqueIdentifier="user.name@notimplemented.com">
+        <ArrowDownFromLine strokeWidth={1} size={18} />
+      </HistoryUserIdentity>
+      <HistoryLocationDetail variant="tracked" locationId={event.data.locationId} locationName={event.data.locationName} date={event.date} />
     </HistoryRow>
   )
 }
@@ -115,8 +121,14 @@ function ItemTrackedEventDetail({ event }: { event: ItemTrackedEvent }) {
 function ItemCreatedEventDetail({ event }: { event: ItemCreatedEvent }) {
   return (
     <HistoryRow>
-      <HistoryUserIdentity userName={event.userName} uniqueIdentifier="user.name@notimplemented.com" />
-      <HistoryLocationDetail variant="created" locationName={event.data.locationName} date={event.date} />
+      <HistoryUserIdentity
+        userName={event.userName}
+        uniqueIdentifier="user.name@notimplemented.com"
+        className="bg-green-400/60"
+      >
+        <Box strokeWidth={1} size={18}/>
+      </HistoryUserIdentity>
+      <HistoryLocationDetail variant="created" locationId={event.data.locationId} locationName={event.data.locationName} date={event.date} />
     </HistoryRow>
   )
 }
