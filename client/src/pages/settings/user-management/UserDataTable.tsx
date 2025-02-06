060873d0710c93a28ab0aea2279f1e16ab64057f
Mike Murphy <michael.w.murphy@outlook.com>
2025-02-06 16:19:43 +0000

    Adds pagination to user data table

--- client/src/pages/settings/user-management/UserDataTable.tsx
+++ client/src/pages/settings/user-management/UserDataTable.tsx
@@ -147,73 +147,96 @@ export function UserDataTable({ data, isLoading, filteredRoles, onFilteredRolesC
   }
 
   return (
-    <div className="rounded-md border">
-      <section className="flex justify-between items-center p-2">
-        <Input
-          placeholder="Search users by name"
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
-          onChange={(e) => table.getColumn("name")?.setFilterValue(e.target.value)}
-          className="max-w-sm sm:max-w-md"
-        />
-        <div className={cn("flex gap-2")}>
-          {ALL_ROLES.map((role) => (
-            <RoleBadge
-              key={role}
-              role={role}
-              onClick={handleRoleFilterClicked}
-              tooltipOff={true}
-              className={cn("", !filteredRoles.includes(role) && "bg-muted text-slate-800")} />
-          ))}
-        </div>
-      </section>
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(
-                      cell.column.columnDef.cell,
-                      cell.getContext()
-                    )}
-                  </TableCell>
-                ))}
+    <div className="w-full">
+      <div className="rounded-md border">
+        <section className="flex justify-between items-center p-2">
+          <Input
+            placeholder="Search users by name"
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(e) => table.getColumn("name")?.setFilterValue(e.target.value)}
+            className="max-w-sm sm:max-w-md"
+          />
+          <div className={cn("flex gap-2")}>
+            {ALL_ROLES.map((role) => (
+              <RoleBadge
+                key={role}
+                role={role}
+                onClick={handleRoleFilterClicked}
+                tooltipOff={true}
+                className={cn("", !filteredRoles.includes(role) && "bg-muted text-slate-800")} />
+            ))}
+          </div>
+        </section>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell
-                colSpan={columns.length}
-                className="h-24 text-center"
-              >
-                {isLoading ? "Loading users..." : "No content"}
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  {isLoading ? "Loading users..." : "No content"}
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <div className="flex-1 text-sm text-muted-foreground">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row{table.getFilteredRowModel().rows.length > 1 ? "s" : null} selected.
+        </div>
+        <div className="space-x-2">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
